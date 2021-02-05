c4597f2220897c05dcfe66a7bc2d1935a1d852ff
Ralph Lee <ralph.lee@live.ca>
2021-02-05 17:42:55 +0900

    Refactor code with new constants

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({this.icon, this.label});
@@ -18,7 +19,7 @@ class IconContent extends StatelessWidget {
         SizedBox(height: 15),
         Text(
           label,
-          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+          style: kLabelTextStyle,
         )
       ],
     );
