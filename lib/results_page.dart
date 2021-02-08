3818f9b9854c8609441de5f1a7ffabd6e6f9536d
Ralph Lee <ralph.lee@live.ca>
2021-02-09 01:40:38 +0900

    Add ReusableCard for presenting results

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,7 +9,66 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("BMI CALCULATOR"),
       ),
-      body: Text("Hi"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                "Your Result",
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'OVERWEIGHT',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.6',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'You are fatass',
+                    style: kResultTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return ResultsPage();
+                }),
+              );
+            },
+            child: Container(
+              child: Center(
+                child: Text(
+                  "Try Again",
+                  style: kLargeButtonTextStyle,
+                ),
+              ),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
